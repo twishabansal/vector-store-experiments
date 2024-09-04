7e191cffb63ff0392fea6a1ba743dd339cdb4763
Twisha Bansal <twishabansal@google.com>
2024-09-04 12:17:37 +0530

    Load all data from chroma

--- chroma_db/app.py
+++ chroma_db/app.py
@@ -5,27 +5,33 @@ chroma_client = chromadb.PersistentClient(path="db")
 
 # chroma_client = chromadb.Client()
 
-collection = chroma_client.create_collection(name="my_collection3")
-collection2 = chroma_client.create_collection(name="my_collection2")
+# collection = chroma_client.create_collection(name="my_collection3")
+# collection2 = chroma_client.create_collection(name="my_collection2")
 
-collection.add(
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about oranges"
-    ],
-    ids=["id1", "id2"]
-)
+# collection.add(
+#     documents=[
+#         "This is a document about pineapple",
+#         "This is a document about oranges"
+#     ],
+#     ids=["id1", "id2"]
+# )
 
-collection2.add(
-    documents=[
-        "This is a document about phones",
-        "This is a document about laptops"
-    ],
-    ids=["id12", "id22"]
-)
+# collection2.add(
+#     documents=[
+#         "This is a document about phones",
+#         "This is a document about laptops"
+#     ],
+#     ids=["id12", "id22"]
+# )
 
-results = collection.query(
-    query_texts=["This is a query document about hawaii"], # Chroma will embed this for you
-    n_results=2 # how many results to return
-)
-print(results)
+# results = collection.query(
+#     query_texts=["This is a query document about hawaii"], # Chroma will embed this for you
+#     n_results=2 # how many results to return
+# )
+# print(results)
+
+collection = chroma_client.get_collection(name="my_collection3")
+
+docs = collection.get()
+for doc in docs:
+    print(f"{doc}: {docs[doc]}")
