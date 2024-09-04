3399b7f6aee7603fcd67e22398631656b1ab942e
Twisha Bansal <twishabansal@google.com>
2024-09-04 12:16:55 +0530

    milvus experiment

---
+++ milvus/app.py
@@ -0,0 +1,103 @@
+from langchain_milvus import Milvus
+from langchain_google_vertexai import VertexAIEmbeddings
+from uuid import uuid4
+
+from langchain_core.documents import Document
+
+from langchain_milvus import Milvus
+
+# The easiest way is to use Milvus Lite where everything is stored in a local file.
+# If you have a Milvus server you can use the server URI such as "http://localhost:19530".
+URI = "./milvus_example_collections.db"
+
+embeddings = VertexAIEmbeddings(
+    model_name="textembedding-gecko@003", project="twisha-dev"
+)
+
+vector_store = Milvus(
+    embedding_function=embeddings,
+    connection_args={"uri": URI},
+    collection_name="test_collection",
+)
+# document_1 = Document(
+#     page_content="I had chocalate chip pancakes and scrambled eggs for breakfast this morning.",
+#     metadata={"source": "tweet"},
+# )
+
+# document_2 = Document(
+#     page_content="The weather forecast for tomorrow is cloudy and overcast, with a high of 62 degrees.",
+#     metadata={"source": "news"},
+# )
+
+# document_3 = Document(
+#     page_content="Building an exciting new project with LangChain - come check it out!",
+#     metadata={"source": "tweet"},
+# )
+
+# document_4 = Document(
+#     page_content="Robbers broke into the city bank and stole $1 million in cash.",
+#     metadata={"source": "news"},
+# )
+
+# document_5 = Document(
+#     page_content="Wow! That was an amazing movie. I can't wait to see it again.",
+#     metadata={"source": "tweet"},
+# )
+
+# document_6 = Document(
+#     page_content="Is the new iPhone worth the price? Read this review to find out.",
+#     metadata={"source": "website"},
+# )
+
+# document_7 = Document(
+#     page_content="The top 10 soccer players in the world right now.",
+#     metadata={"source": "website"},
+# )
+
+# document_8 = Document(
+#     page_content="LangGraph is the best framework for building stateful, agentic applications!",
+#     metadata={"source": "tweet"},
+# )
+
+# document_9 = Document(
+#     page_content="The stock market is down 500 points today due to fears of a recession.",
+#     metadata={"source": "news"},
+# )
+
+# document_10 = Document(
+#     page_content="I have a bad feeling I am going to get deleted :(",
+#     metadata={"source": "tweet"},
+# )
+
+# documents = [
+#     document_1,
+#     document_2,
+#     document_3,
+#     document_4,
+#     document_5,
+#     document_6,
+#     document_7,
+#     document_8,
+#     document_9,
+#     document_10,
+# ]
+# uuids = [str(uuid4()) for _ in range(len(documents))]
+
+# vector_store.add_documents(documents=documents, ids=uuids)
+
+results = vector_store.similarity_search(
+    "LangChain provides abstractions to make working with LLMs easy",
+    k=2,
+    filter={"source": "tweet"},
+)
+for res in results:
+    print(f"* {res.page_content} [{res.metadata}]")
+
+vector_store_loaded = Milvus(
+    embeddings,
+    connection_args={"uri": URI},
+    collection_name="langchain_example",
+)
+for doc in vector_store_loaded:
+    print(doc)
+# print(vector_store_loaded)
